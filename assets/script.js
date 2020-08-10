8d9f8d420a9249937a1a6991a8aef5c6aa29fb1e
Alissa Champigny <achampigny4@gmail.com>
2020-08-09 23:26:35 -0400

    dynamically create city buttons

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ $(function () {
     const currentDate = moment().format("MM/DD/YYYY");
     //current weather icon
     let cityWeatherIcon = $("#current-icon");
+    // Buttons for the search history
+    let searchedCityHistory = $('#searched-city-btn');
 
     //search button on click function
     $("button").on("click", function () {
@@ -13,9 +15,6 @@ $(function () {
         //search input
         let city = $('#search').val().trim();
 
-        //console.log/////////////////////////////////////////////////////
-        // console.log(city);
-
         //store city API URL for weather
         let cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9a59b92bab7df562dc4f082669ec387d`
         // console.log(cityURL); ///////////////////////////////////////////////
@@ -23,59 +22,58 @@ $(function () {
         // GET request to cityURL
         $.get(cityURL).then(function (response) {
 
-            // console.log/////////////////////////////////////////////////
-            // console.log(response);
-
-
             //empty search input alert
             if (city === "") {
                 alert("Please add City, State, USA or City, Country");
             } else {
-                // (storeCity('city')) 
-                //store city to local storage
-                window.localStorage.setItem('city', JSON.stringify(city));
-            }
+                //store city searched to local storage
+                window.localStorage.setItem('cityHistory', JSON.stringify(city));
+            };
 
-        //City
-        let currentCity = response.name;
-        // weather icon 
-        let weatherIcon = response.weather[0].icon;
-        let weatherIconURL = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
-        //weather description
-        let currentWeather = response.weather[0].description;
-        //temperature converted to fahrenheit
-        let tempF = Math.floor((response.main.temp - 273.15) * 1.80 + 32);
-        //Humidity percentage
-        let currentHumidity = response.main.humidity;
-        //Current wind speed
-        let currentWind = response.wind.speed;
+            //City name
+            let currentCity = response.name;
+            // weather icon 
+            let weatherIcon = response.weather[0].icon;
+            let weatherIconURL = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
+            //weather description
+            let currentWeather = response.weather[0].description;
+            //temperature converted to fahrenheit
+            let tempF = Math.floor((response.main.temp - 273.15) * 1.80 + 32);
+            //Humidity percentage
+            let currentHumidity = response.main.humidity;
+            //Current wind speed
+            let currentWind = response.wind.speed;
 
-        //display results
-        $("#current-city").text(currentCity + " (" + currentDate + ")");
-        $("#current-icon").append(cityWeatherIcon.attr("src", weatherIconURL));
-        $("#current-description").text(currentWeather);
-        $("#current-temp").text("Temperature: " + tempF + " °F");
-        $("#current-humidity").text("Humidity: " + currentHumidity + " %");
-        $("#current-wind").text("Wind speed: " + currentWind + " MPH");
+            //display results
+            $("#current-city").text(currentCity + " (" + currentDate + ")");
+            $("#current-icon").append(cityWeatherIcon.attr("src", weatherIconURL));
+            $("#current-description").text(currentWeather);
+            $("#current-temp").text("Temperature: " + tempF + " °F");
+            $("#current-humidity").text("Humidity: " + currentHumidity + " %");
+            $("#current-wind").text("Wind speed: " + currentWind + " MPH");
 
-        //create button of searched city
-        // cityButton();
+            //create button of searched city
+            createCityButton();
+        });
     });
-});
 
-//global var after save to localStorage
-// city history for buttons
-var city = JSON.parse(window.localStorage.getItem("city")) || [];
-    // Buttons for the search history
-    // const searchedCityBtn = $('#searched-city-btn');
+    //global var after save to localStorage
+    // city history for buttons
+    let city = JSON.parse(window.localStorage.getItem("cityHistory")) || [];
 
+    function createCityButton() {
+        for (let i = 0; i < localStorage.length; i++){
+        city = localStorage.getItem("cityHistory" + i);
+        $("#searched-city-btn").append('<button class="btn waves-effect waves-light blue" type="submit"/>');
+        };
+    };
+
+    // Display Buttons of past cities searched when the page loads
+    // createCityButton();
 
     // 2    //on click city is added to url to use api for displaying 5 day forecast
     // 3    //on click city is dynamically made into a button below search
-    //move function above on click of sumbit button
-    // function cityButton(){
-    // };
-    // 5    //on click previous searched city will display current and 5 day forecasts    
+    // 4    //on click previous searched city buttons display current weather and 5 day forecast  
 
     // end code
 });
